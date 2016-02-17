bfbe1494b3c76a21d036825258d277bc862ddd82
Marlise <marlise.au@gmail.com>
2016-02-16 20:02:01 -0500

    Implemented login and register controllers, register form

---
+++ cs50xapp-git/includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "sec_user");    // The database username.
+define("PASSWORD", "6ZaxN2Vzm9NUJT2y");    // The database password.
+define("DATABASE", "cs50xapp");    // The database name.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
--- cs50xapp/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    // configuration
-    //require("../includes/config.php");
-	require("../includes/functions.php");
-    render("../templates/home.php", [$title = "Home"]);
-	
-?>
