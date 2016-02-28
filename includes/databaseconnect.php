3eed79acdbb086745181325349d3c544822b69e4
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-02-28 16:00:29 -0500

    Protection against going to logged-in.php without logging in

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -14,6 +14,6 @@
   //
   // // Storing Session
   if (session_status() == PHP_SESSION_NONE) {
-      session_start();
+    session_start();
   }
 ?>
--- logged-in.php
+++ logged-in.php
@@ -1,11 +1,8 @@
 <?php
-
+if (session_status() == PHP_SESSION_NONE) {
+  header("location: index.php"); // Redirect to homepage
+}
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
