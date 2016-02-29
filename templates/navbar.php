7a433d1467f80a32fa1e2369ce68dc69dc7e7b79
marliseu <marlise.au@gmail.com>
2016-02-28 20:10:15 -0500

    Modified navbar to be more mobile friendly

--- templates/navbar.php
+++ templates/navbar.php
@@ -15,7 +15,7 @@
 				<!--================================-->
 				<div class="navbar-header">
 					<a href="index.php" class="navbar-brand">
-						<img src="img/CS50xMiami_Logo.png" alt="CS50xMiami Logo" class="brand-icon">
+						<img src="img/CS50xMiami_Logo.png" alt="CS50xMiami Logo" class="img-circle brand-icon">
 						<div class="brand-title">
 							<span class="brand-text">CS50xMiami</span>
 						</div>
@@ -50,21 +50,11 @@
 								<span class="pull-right">
 									<img class="img-circle img-user media-object" src="img/av1.png" alt="Profile Picture">
 								</span>
-								<div class="username hidden-xs"><?= $_SESSION['fname'] . " " . $_SESSION['lname'] ?></div>
+								<div class="username"><?= $_SESSION['fname'] . " " . $_SESSION['lname'] ?></div>
 							</a>
 
 
-							<div class="dropdown-menu dropdown-menu-md dropdown-menu-right with-arrow panel-default">
-
-								<!-- Dropdown heading  -->
-								<div class="pad-all bord-btm">
-									<p class="text-lg text-muted text-thin mar-btm">750Gb of 1,000Gb Used</p>
-									<div class="progress progress-sm">
-										<div class="progress-bar" style="width: 70%;">
-											<span class="sr-only">70%</span>
-										</div>
-									</div>
-								</div>
+							<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right with-arrow panel-default">
 
 
 								<!-- User dropdown menu -->
@@ -74,15 +64,14 @@
 											<i class="fa fa-user fa-fw fa-lg"></i> Profile
 										</a>
 									</li>
-									<li>
+									<!-- <li>
 										<a href="#">
 											<span class="badge badge-danger pull-right">9</span>
 											<i class="fa fa-envelope fa-fw fa-lg"></i> Messages
 										</a>
-									</li>
+									</li> -->
 									<li>
 										<a href="#">
-											<span class="label label-success pull-right">New</span>
 											<i class="fa fa-gear fa-fw fa-lg"></i> Settings
 										</a>
 									</li>
@@ -91,15 +80,10 @@
 											<i class="fa fa-question-circle fa-fw fa-lg"></i> Help
 										</a>
 									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-lock fa-fw fa-lg"></i> Lock screen
-										</a>
-									</li>
 								</ul>
 
 								<!-- Dropdown footer -->
-								<div class="pad-all text-right">
+								<div class="pad-all text-center">
 									<a href="log-out.php" class="btn btn-primary">
 										<i class="fa fa-sign-out fa-fw"></i> Logout
 									</a>
