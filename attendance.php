3aab8d69edc48bd98077548ba08316b2fb449863
marliseu <marlise.au@gmail.com>
2016-03-05 13:16:32 -0500

    Separated QR code from template and put in controller

--- attendance.php
+++ attendance.php
@@ -4,6 +4,7 @@
 // }
 require('includes/databaseconnect.php');
 require('includes/user.php');
+include('includes/phpqrcode/qrlib.php');
 
 if($_SESSION['id'] == ''){
     header("location:index.php");
@@ -11,6 +12,30 @@ if($_SESSION['id'] == ''){
 
 // Set dynamic page title
 $title = 'Attendance';
+
+
+// Get time
+date_default_timezone_set("America/New_York"); // Miami time!
+$t=time();
+$currentDate = date("m/d/y", $t);
+$currentTime = date("h:i:s");
+
+// Text to be stored on QR code, in format for CSV file
+$textForDisplay = $_SESSION['fname'] . "," . $_SESSION['lname'] . "," . $_SESSION['email'] . "," . $currentDate . "," . $currentTime;
+
+// QR info
+$size = 4;
+$padding = 2;
+
+// Set up directories + filepaths.
+$qrDir = "qrcodes/" . date("m-d-y", $t) . "/";
+$filePath = $qrDir . $_SESSION['fname'] . " " . $_SESSION['lname'] . ".png";
+if (!file_exists($qrDir)) {
+  mkdir($qrDir, 0777, true);
+}
+
+// Generate & display QR code. Saves QR to folder for backup.
+QRcode::png($textForDisplay, $filePath, QR_ECLEVEL_H, $size, $padding);
 
 // Render templates
 require ("includes/databaseconnect.php");
