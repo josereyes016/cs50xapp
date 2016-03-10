53d75e275004a7410df3e272f113bd2fb89e8f6a
marliseu <marlise.au@gmail.com>
2016-03-09 23:40:17 -0500

    Added facebook and twitter feed to dashboard

--- templates/dashboard-page.php
+++ templates/dashboard-page.php
@@ -29,21 +29,6 @@
 
 
       <div class="col-sm-12 col-xs-12">
-        <div class="panel panel-info">
-        <!-- Panel heading -->
-        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-          <div class="panel-heading">
-            <h3 class="panel-title">Panel</h3>
-          </div>
-          <!-- Panel body -->
-          <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-          <div class="panel-body">
-              <!-- Content here -->
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-8 col-xs-12">
         <div class="panel panel-mint">
         <!-- Panel heading -->
         <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
@@ -58,17 +43,37 @@
         </div>
       </div>
 
-      <div class="col-sm-4 col-xs-12">
-        <div class="panel panel-default">
+      <div class="col-sm-6 col-xs-12">
+        <div class="panel panel-primary">
         <!-- Panel heading -->
         <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
           <div class="panel-heading">
-            <h3 class="panel-title">Panel</h3>
+            <h3 class="panel-title">Facebook</h3>
           </div>
           <!-- Panel body -->
           <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
           <div class="panel-body">
-              <!-- Content here -->
+            <!-- Content here -->
+            <div class="container">
+              <div class="fb-page" data-href="https://www.facebook.com/CS50xMIAMI-871833776233275/" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-xs-12">
+        <div class="panel panel-info">
+        <!-- Panel heading -->
+        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+          <div class="panel-heading">
+            <h3 class="panel-title">Twitter</h3>
+          </div>
+          <!-- Panel body -->
+          <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+          <div class="panel-body">
+            <!-- Content here -->
+            <a class="twitter-timeline" href="https://twitter.com/cs50xMiami" data-widget-id="707787725890523142">Tweets by @cs50xMiami</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>  
           </div>
         </div>
       </div>
