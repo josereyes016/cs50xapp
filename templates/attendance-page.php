70e4a794e1f8b3146be0a81c9b6fd6dac9c1f2a1
marliseu <marlise.au@gmail.com>
2016-03-12 14:01:07 -0500

    Umad CSS

--- templates/attendance-page.php
+++ templates/attendance-page.php
@@ -26,7 +26,7 @@
     <!--Page content-->
     <!--===================================================-->
     <div id="page-content">
-      <div class="panel col-xs-12">
+      <div class="panel col-sm-12">
 				<div class="panel-body">
           <img src="<?= $filePath ?>">
           <hr />
