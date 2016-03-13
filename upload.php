00ab068859ca2a488bc60c056906ffb107f15cf6
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-03-13 12:05:38 -0400

    Filetype verification + only images allowed for uploading

--- upload.php
+++ upload.php
@@ -12,26 +12,28 @@ if (!file_exists($storeFolder)) {
 
 if (!empty($_FILES)) {
   $tempFile = $_FILES['file']['tmp_name']; //If file is sent to the page, store the file object to a temporary variable.
-  $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;  //Create the absolute path of the destination folder.
-  $targetFile =  $targetPath . $_FILES['file']['name'];  //Create the absolute path of the uploaded file destination.
 
-  $extension = "";
-  if ($_FILES["file"]["type"] == "image/jpeg") {
-    $extension = ".jpg";
+  if (getimagesize($tempFile) != false) { // verify file is actually an image
+    $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;  //Create the absolute path of the destination folder.
+    $targetFile =  $targetPath . $_FILES['file']['name'];  //Create the absolute path of the uploaded file destination.
+
+    $extension; // capture file extension
+    if ($_FILES["file"]["type"] == "image/jpeg") {
+      $extension = ".jpg";
+    }
+    elseif ($_FILES["file"]["type"] == "image/png") {
+      $extension = ".png";
+    }
+    elseif ($_FILES["file"]["type"] == "image/gif") {
+      $extension = ".gif";
+    }
+
+    $fileName = "notWorking"; // TODO image name should be user id
+    $avatar = $targetPath . $fileName . $extension;
+    move_uploaded_file($tempFile,$avatar); //Move uploaded file to destination.
+
+    //TODO SQL query to add img name (or path?) to db
   }
-  elseif ($_FILES["file"]["type"] == "image/png") {
-    $extension = ".png";
-  }
-  elseif ($_FILES["file"]["type"] == "image/gif") {
-    $extension = ".gif";
-  }
-
-  //TODO verify file is actually an image. getimagesize?
-
-  $fileName = "working";
-  $avatar = $targetPath . $fileName . $extension;
-
-  move_uploaded_file($tempFile,$avatar); //Move uploaded file to destination.
 }
 header("location: editprofile.php");
 
