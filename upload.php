0f799792026330071acf7ce7d223f03339681d31
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-03-13 11:20:59 -0400

    Uploaded files retain file extension if image!
    TODO: die if not image. verify file is image. cry with happiness that
    this works.

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ include('user.php');
 
 // Source: http://www.startutorial.com/articles/view/how-to-build-a-file-upload-form-using-dropzonejs-and-php
 $ds = DIRECTORY_SEPARATOR;  //Store directory separator (DIRECTORY_SEPARATOR) to a simple variable
-$storeFolder = 'img/profiles';  //Declare a variable for destination folder.
+$storeFolder = 'img' . $ds . 'profiles';  //Declare a variable for destination folder.
 
 if (!file_exists($storeFolder)) {
   mkdir($storeFolder, 0750, true);
@@ -14,7 +14,24 @@ if (!empty($_FILES)) {
   $tempFile = $_FILES['file']['tmp_name']; //If file is sent to the page, store the file object to a temporary variable.
   $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;  //Create the absolute path of the destination folder.
   $targetFile =  $targetPath . $_FILES['file']['name'];  //Create the absolute path of the uploaded file destination.
-  move_uploaded_file($tempFile,$targetFile); //Move uploaded file to destination.
+
+  $extension = "";
+  if ($_FILES["file"]["type"] == "image/jpeg") {
+    $extension = ".jpg";
+  }
+  elseif ($_FILES["file"]["type"] == "image/png") {
+    $extension = ".png";
+  }
+  elseif ($_FILES["file"]["type"] == "image/gif") {
+    $extension = ".gif";
+  }
+
+  //TODO verify file is actually an image. getimagesize?
+
+  $fileName = "working";
+  $avatar = $targetPath . $fileName . $extension;
+
+  move_uploaded_file($tempFile,$avatar); //Move uploaded file to destination.
 }
 header("location: editprofile.php");
 
