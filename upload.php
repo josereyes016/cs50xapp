918c51c1736c272055cedb7a6a17cb79c7c24108
marliseu <marlise.au@gmail.com>
2016-04-02 23:37:57 -0400

    Merge branch 'master' into grades
    # Conflicts:
    #	qrcodes/13.png
    #	upload.php

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@ $userID = $user['id'];
 
 // Source: http://www.startutorial.com/articles/view/how-to-build-a-file-upload-form-using-dropzonejs-and-php
 $ds = DIRECTORY_SEPARATOR;  //Store directory separator (DIRECTORY_SEPARATOR) to a simple variable
-$storeFolder = 'uploads';   //Declare a variable for destination folder.
+$storeFolder = 'img' . $ds . 'profiles';  //Declare a variable for destination folder.
 
 if (!file_exists($storeFolder)) {
   mkdir($storeFolder, 0750, true);
@@ -14,12 +14,37 @@ if (!file_exists($storeFolder)) {
 
 if (!empty($_FILES)) {
   $tempFile = $_FILES['file']['tmp_name']; //If file is sent to the page, store the file object to a temporary variable.
+<<<<<<< HEAD
   $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;  //Create the absolute path of the destination folder.
   $targetFile =  $targetPath . $_FILES['file']['name'];  //Create the absolute path of the uploaded file destination.
   move_uploaded_file($tempFile,$targetFile); //Move uploaded file to destination.
 
   mysqli_query($db, "UPDATE `profile` SET `avatar_path`='$targetPath' WHERE `id`='$userID'");
   $user['avatar_path'] = $targetPath;
+=======
+
+  if (getimagesize($tempFile) != false) { // verify file is actually an image
+    $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;  //Create the absolute path of the destination folder.
+    $targetFile =  $targetPath . $_FILES['file']['name'];  //Create the absolute path of the uploaded file destination.
+
+    $extension; // capture file extension
+    if ($_FILES["file"]["type"] == "image/jpeg") {
+      $extension = ".jpg";
+    }
+    elseif ($_FILES["file"]["type"] == "image/png") {
+      $extension = ".png";
+    }
+    elseif ($_FILES["file"]["type"] == "image/gif") {
+      $extension = ".gif";
+    }
+
+    $fileName = "notWorking"; // TODO image name should be user id
+    $avatar = $targetPath . $fileName . $extension;
+    move_uploaded_file($tempFile,$avatar); //Move uploaded file to destination.
+
+    //TODO SQL query to add img name (or path?) to db
+  }
+>>>>>>> master
 }
 
 header("location: editprofile.php");
