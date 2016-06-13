09dcb710d5c9c2532cd018b33285709e5b86e534
Marlise Urscheler <marlise.au@gmail.com>
2016-06-13 15:54:52 -0400

    Updated calendar embed

--- templates/calendar-page.php
+++ templates/calendar-page.php
@@ -28,7 +28,7 @@
     <div id="page-content">
 
       <div class="responsive-iframe-container">
-            <iframe src="https://calendar.google.com/calendar/embed?title=Cohort%205&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="yes"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?src=02r6c0anpj2bgtuhh4tcnv66b0%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
 
       <script type='text/javascript'>
