894d471364fbc1fbf505c511926f9130e81abeb6
josereyes016 <codesmash50@gmail.com>
2016-06-14 14:10:32 -0400

    Modified add_tf_to_student to return a bool

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -19,33 +19,30 @@
     session_start();
   }
   
-  function add_tf_to_student( $database, $email, $user, &$error, &$success)
-  {
-      if ($email == ''){
-        $error = "Please enter a student's e-mail address.";
-      }
-      else {
-        $studentEmail_cleaned = trim(strtolower($email));
-        $studentQuery = mysqli_query($database, "SELECT *
-                                      FROM `users`
-                                     WHERE `email` = '$studentEmail_cleaned'");
-        $studentRows = mysqli_num_rows($studentQuery);
-        if ($studentRows != 1) {
-          $error = "Invalid e-mail address.";
-        } else {
-          $studentInfo = mysqli_fetch_assoc($studentQuery);
-          if ($studentInfo['is_admin'] == 1){
-            $error = "User is an administrator.";
-          } else {
-            mysqli_query($database, "UPDATE `users`
-                                  SET `tf`='$user'
-                                WHERE `email`='$studentEmail_cleaned'");
-            $error = '';
-            $success = "Student was successfully added.";
-            header("location: grades.php");
-          }
-        }
-      }
+function add_tf_to_student( $database, $email, $user)
+{
+    
+  $studentEmail_cleaned = trim(strtolower($email));
+
+  $studentQuery = mysqli_query($database, "SELECT *
+                                FROM `users`
+                               WHERE `email` = '$studentEmail_cleaned'");
+  $studentRows = mysqli_num_rows($studentQuery);
+
+  if ($studentRows != 1) {
+    return true;
+  } 
+  else {
+    $studentInfo = mysqli_fetch_assoc($studentQuery);
+
+    if ($studentInfo['is_admin'] == 1){
+      return true;
+    } 
+    else {
+      return mysqli_query($database, "UPDATE `users`
+                            SET `tf`='$user'
+                          WHERE `email`='$studentEmail_cleaned'");
     }
   }
+}
 ?>
