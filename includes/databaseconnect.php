022f3ee21eb61a3b643e1afb40c5a541596f63ef
josereyes016 <codesmash50@gmail.com>
2016-06-14 11:50:38 -0400

    Created function to add tf to a particular user

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -18,4 +18,34 @@
   if (session_status() == PHP_SESSION_NONE) {
     session_start();
   }
+  
+  function add_tf_to_student( $database, $email, $user)
+  {
+      if ($email == ''){
+        $formError = "Please enter a student's e-mail address.";
+      }
+      else {
+        $studentEmail_cleaned = trim(strtolower($email));
+        $studentQuery = mysqli_query($database, "SELECT *
+                                      FROM `users`
+                                     WHERE `email` = '$studentEmail_cleaned'");
+        $studentRows = mysqli_num_rows($studentQuery);
+        if ($studentRows != 1) {
+          $formError = "Invalid e-mail address.";
+        } else {
+          $studentInfo = mysqli_fetch_assoc($studentQuery);
+          if ($studentInfo['is_admin'] == 1){
+            $formError = "User is an administrator.";
+          } else {
+            mysqli_query($database, "UPDATE `users`
+                                  SET `tf`='$user'
+                                WHERE `email`='$studentEmail_cleaned'");
+            $formError = '';
+            $formSuccess = "Student was successfully added.";
+            header("location: grades.php");
+          }
+        }
+      }
+    }
+  }
 ?>
