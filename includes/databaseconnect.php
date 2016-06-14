fe919a5b33b53dd03282230bc5b7cb61ddfb457b
josereyes016 <codesmash50@gmail.com>
2016-06-14 12:27:43 -0400

    added references to function for resutls

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -19,10 +19,10 @@
     session_start();
   }
   
-  function add_tf_to_student( $database, $email, $user)
+  function add_tf_to_student( $database, $email, $user, &$error, &$success)
   {
       if ($email == ''){
-        $formError = "Please enter a student's e-mail address.";
+        $error = "Please enter a student's e-mail address.";
       }
       else {
         $studentEmail_cleaned = trim(strtolower($email));
@@ -31,17 +31,17 @@
                                      WHERE `email` = '$studentEmail_cleaned'");
         $studentRows = mysqli_num_rows($studentQuery);
         if ($studentRows != 1) {
-          $formError = "Invalid e-mail address.";
+          $error = "Invalid e-mail address.";
         } else {
           $studentInfo = mysqli_fetch_assoc($studentQuery);
           if ($studentInfo['is_admin'] == 1){
-            $formError = "User is an administrator.";
+            $error = "User is an administrator.";
           } else {
             mysqli_query($database, "UPDATE `users`
                                   SET `tf`='$user'
                                 WHERE `email`='$studentEmail_cleaned'");
-            $formError = '';
-            $formSuccess = "Student was successfully added.";
+            $error = '';
+            $success = "Student was successfully added.";
             header("location: grades.php");
           }
         }
